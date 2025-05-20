49f0205a52c9bd266c68e85874257380bd0a29bc
DIEGO POSADA <dapiposada@gmail.com>
2025-05-20 16:10:21 -0600

    feat:endpoint para obtenerinformacion del archivo subido

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+//heeramienta para informaciond leer el archivo
+const multer  = require('multer')
+const upload = multer()
 
 var app = express();
 
@@ -12,6 +15,15 @@ app.get('/', function (req, res) {
 });
 
 
+//endopoint para extraer informacion de archivo
+app.route("/api/fileanalyse")
+.post(upload.single('upfile'), async(req, res)=>{
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
